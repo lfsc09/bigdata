8c4e0fbbc5dcb4a85ff55e47c298741380a78a55
lfsc09 <lfsc09@gmail.com>
2017-04-07 06:08:11 -0700

    Update exercise02.py

--- map-reduce/exercises/exercise02.py
+++ map-reduce/exercises/exercise02.py
@@ -8,10 +8,9 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
  	conf = SparkConf().setAppName("Exercise 02")
  	sc = SparkContext(conf=conf)
-	rdd = sc.wholeTextFiles(sys.argv[1]).cache()
-	files = rdd.collect()
-	words = sc.parallelize(files[0][1].split()).map(lambda _: (0, 1)).reduceByKey(lambda _1,_2: _1+_2).map(lambda _: (files[0][0], _[1]))
-	for (filename, content) in files[1:]:
-		words_file = sc.parallelize(content.split()).map(lambda _: (0, 1)).reduceByKey(lambda _1,_2: _1+_2).map(lambda _: (filename, _[1]))
+	files = sc.wholeTextFiles(sys.argv[1]).map(lambda (filename, content): filename).collect()
+	words = sc.textFile(files[0]).flatMap(lambda line: line.split()).map(lambda _: (0, 1)).reduceByKey(lambda _1,_2: _1+_2).map(lambda _: (files[0], _[1]))
+	for filename in files[1:]:
+		words_file = sc.textFile(filename).flatMap(lambda line: line.split()).map(lambda _: (0, 1)).reduceByKey(lambda _1,_2: _1+_2).map(lambda _: (filename, _[1]))
 		words = words.union(words_file)
 	words.saveAsTextFile("exercise02-out")
