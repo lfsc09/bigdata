68f2b1b02e7a6b3c5e25baca37fc3c1aa655e21c
lfsc09 <lfsc09@gmail.com>
2017-04-07 06:08:52 -0700

    Update exercise03.py

--- map-reduce/exercises/exercise03.py
+++ map-reduce/exercises/exercise03.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
  	conf = SparkConf().setAppName("Exercise 03")
  	sc = SparkContext(conf=conf)
 	given_word = sys.argv[2]
-	rdd = sc.wholeTextFiles(sys.argv[1]).cache()
-	files = rdd.collect()
+	files = sc.wholeTextFiles(sys.argv[1]).map(lambda (filename, content): filename).collect()
 	places_found = []
-	for (filename, content) in files:
-		places_found.append(sc.parallelize(content.split()).map(lambda word: (word, filename)).reduceByKey(lambda _1,_2: _1).sortByKey().lookup(given_word))
+	for filename in files:
+		places_found.append(sc.textFile(filename).map(lambda word: (word, filename)).reduceByKey(lambda _1,_2: _1).sortByKey().lookup(given_word))
 	sc.parallelize(places_found).flatMap(lambda _: _).saveAsTextFile("exercise03-out")
