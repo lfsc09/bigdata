467fb757d789d1a6aea46cb755ba3c4b1beb888a
lfsc09 <lfsc09@gmail.com>
2017-05-04 06:50:06 -0700

    Update trabalho_mapreduce.py

--- map-reduce/trabalho_mapreduce.py
+++ map-reduce/trabalho_mapreduce.py
@@ -1,6 +1,6 @@
-#spark-submit --master yarn --deploy-mode client --executor-memory 1g --name trabalho --conf "spark.app.id=trabalho" trabalho_mapreduce.py 'dir_name|filename' 'url_number'
+#spark-submit --master yarn --deploy-mode client --executor-memory 1g --name trabalho --conf "spark.app.id=trabalho" trabalho_mapreduce.py 'dir_name|filename'
 #
-#	Show the files that have more than 'url_number' repeated url links (Mostra os arquivos que tem mais de 'url_number' urls repetidas)
+#	Show the url links that are repeated in files (Mostra as urls que se repetem em arquivos)
 #
 import sys
 from pyspark import SparkContext, SparkConf
@@ -8,13 +8,5 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
  	conf = SparkConf().setAppName("Trabalho")
  	sc = SparkContext(conf=conf)
-	url_number = int(sys.argv[2])
-	url_tests = ["http", "www", ".com", ".br", ".org", "gov"]
-	rdd_files = sc.wholeTextFiles(sys.argv[1]).cache()
-	files = rdd_files.map(lambda (filename, content): filename).collect()
-	places_found = []
-	for filename in files:
-		rdd = sc.textFile(filename).flatMap(lambda line: line.split()).map(lambda word: (word, (filename, 1)) if any(x in word for x in url_tests) else (0, (0,0))).reduceByKey(lambda _1,_2: (_1[0], _1[1]+_2[1])).filter(lambda _: _[1][1] > url_number).collect()
-		if len(rdd) > 0:
-			places_found.append(rdd[0][1][0])
-	sc.parallelize(places_found).saveAsTextFile("trabalho_mapreduce-out")
+	url_tests = ["http", "www", ".com"]
+	rdd_all = sc.textFile(sys.argv[1]).flatMap(lambda line: line.split()).map(lambda word: (word, 1) if any(x in word for x in url_tests) else (0, 0)).reduceByKey(lambda _1,_2: _1+_2).filter(lambda _: _[1] >= 2).coalesce(2).saveAsTextFile("trabalho_mapreduce-out")
